a75e21a87f2cf9ef74315094d29b1bd9f9451fad
Brian Hays <brian.hays@gmail.com>
2013-09-30 19:12:38 -0400

    add contact page

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -44,5 +44,19 @@ describe "StaticPages" do
     end
   end
 
+  describe "Contact page" do
+
+    it "should have the content 'Contact'" do
+      # Run the generator again with the --webrat flag if you want to use webrat methods/matchers
+      visit '/static_pages/contact'
+      expect(page).to have_content('Contact')
+    end
+
+    it "should have the Contact title" do
+      visit '/static_pages/contact'
+      expect(page).to have_title("Ruby on Rails Tutorial Sample App | Contact")
+    end
+  end
+
 
 end
